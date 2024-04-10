e5b56fabc7c2a033264fc9e535bc29da4892a4c8
Elina Akhmanova <elinka@google.com>
2024-04-10 20:47:40 +0200

    test
    Signed-off-by: Elina Akhmanova <elinka@google.com>

--- pkg/instancegroups/controller_test.go
+++ pkg/instancegroups/controller_test.go
@@ -4,8 +4,13 @@ import (
 	"testing"
 	"time"
 
+	"github.com/go-logr/logr"
+	compute "google.golang.org/api/compute/v1"
 	api_v1 "k8s.io/api/core/v1"
 	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	informerv1 "k8s.io/client-go/informers/core/v1"
+	"k8s.io/client-go/kubernetes/fake"
+	"k8s.io/ingress-gce/pkg/utils"
 )
 
 func TestNodeStatusChanged(t *testing.T) {
@@ -78,4 +83,63 @@ func testNode() *api_v1.Node {
 			},
 		},
 	}
+}
+
+func TestSync(t *testing.T) {
+	config := &ControllerConfig{}
+	informer := informerv1.NewNodeInformer(fake.NewSimpleClientset(), 2*time.Second, utils.NewNamespaceIndexer())
+	config.NodeInformer = informer
+	fakeManager := &IGManagerFake{}
+	config.IGManager = fakeManager
+	config.HasSynced = func() bool {
+		return true
+	}
+
+	controller := NewController(config, logr.Logger{})
+
+	channel := make(chan struct{})
+	go informer.Run(channel)
+	go controller.Run()
+
+	firstNode := testNode()
+	secondNode := testNode()
+	secondNode.Name = "n2"
+
+	informer.GetIndexer().Add(node1)
+	informer.GetIndexer().Add(node2)
+	node1.Annotations["key"] = "true"
+	node2.Annotations["key"] = "true"
+	informer.GetIndexer().Update(node1)
+	informer.GetIndexer().Update(node2)
+	time.Sleep(5 * time.Second)
+	informer.GetIndexer().Update(node2)
+	if len(fakeManager.syncedNodes) != 1 {
+		t.Errorf("synced too many times %d, %v", len(fakeManager.syncedNodes), fakeManager.syncedNodes)
+	}
+}
+
+type IGManagerFake struct {
+	syncedNodes [][]string
+}
+
+func (igmf *IGManagerFake) Sync(nodeNames []string) error {
+	igmf.syncedNodes = append(igmf.syncedNodes, nodeNames)
+	return nil
+}
+
+func (igmf *IGManagerFake) EnsureInstanceGroupsAndPorts(name string, ports []int64) ([]*compute.InstanceGroup, error) {
+	return []*compute.InstanceGroup{}, nil
+}
+
+func (igmf *IGManagerFake) DeleteInstanceGroup(name string) error {
+	return nil
+}
+
+func (igmf *IGManagerFake) Get(name, zone string) (*compute.InstanceGroup, error) {
+	ig := compute.InstanceGroup{}
+	return &ig, nil
+}
+
+func (igmf *IGManagerFake) List() ([]string, error) {
+	return []string{}, nil
 }
